c5a358a5f83cbc798ae20777a474487836d3aa0f
Seiei Higa <hanachin@gmail.com>
2013-10-23 19:42:20 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Howfat::Application.config.secret_key_base = '5fa3b9c33d3e1235b63ab5548ae019a05cb9a4d3e11739d42210f782c1d603de3f33467f6ec70be4b69cb4a34387718f9d34f8acfa7f4741915bc0d61c7aace3'
