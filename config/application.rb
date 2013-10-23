1748a69195a477df1b3b8a8edc776ab3efd86768
Seiei Higa <hanachin@gmail.com>
2013-10-23 21:38:34 +0900

    Add generator setting, stop generates css/js/jbuilder/helper, test with rspec

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module Howfat
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.stylesheets      false
+      g.javascripts      false
+      g.jbuilder         false
+      g.helper           false
+      g.test_framework   :rspec, fixture: false
+    end
   end
 end
