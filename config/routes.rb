5038ed9be2b25f6e56ef354529a1c04a891d6b81
Seiei Higa <hanachin@gmail.com>
2013-10-23 20:54:05 +0900

    Add signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Howfat::Application.routes.draw do
+  root 'pages#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
